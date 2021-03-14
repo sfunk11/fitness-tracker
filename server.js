6075e5d1b7770117d156a34cdcdac6bc305279d4
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-03-14 19:37:54 -0400

    initial server and back end file structure

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/api-routes.js");
+require("./routes/html-routes.js");
+
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
