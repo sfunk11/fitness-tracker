8f52193636e0121a6a190124db811c422ffb7f84
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-03-14 20:03:34 -0400

    added workout model

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const WorkoutSchema = new mongoose.Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a Type of Exercise"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter the Exercise Name"
+      },
+      duration: {
+        type: Number,
+        required: "Enter Time in Minutes"
+      },
+      weight: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
+
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
